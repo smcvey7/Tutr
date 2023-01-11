102420aae4ea83419ba4dd98b57bb8c4253a3c8e
Steven McVey <smcvey1210@gmail.com>
2023-01-11 09:14:42 -0800

    add Login component

--- phase-3-sinatra-react-project-frontend/src/Components/Header.js
+++ phase-3-sinatra-react-project-frontend/src/Components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Login from "./Login";
 
 function Header(){
 
   return(
     <div>
       <h1>Webpage Title</h1>
+      <Login/>
     </div>
   )
 }
---
+++ phase-3-sinatra-react-project-frontend/src/Components/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Login(){
+
+  return(
+    <form>
+      I am a...
+      <select>
+        <option>Student</option>
+        <option>Tutor</option>
+      </select>
+      <input></input>
+      <input type="password" ></input>
+    </form>
+  )
+}
+
+export default Login
