4425d836821a0af05792a624de2fe78248c50ed0
Steven McVey <smcvey1210@gmail.com>
2023-01-15 17:37:45 -0800

    add position of student or tutor to users

--- phase-3-sinatra-react-project-backend/db/migrate/20230110220116_create_students.rb
+++ phase-3-sinatra-react-project-backend/db/migrate/20230110220116_create_students.rb
@@ -1,6 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
     create_table :students do |t|
+      t.string :position
       t.string :name
       t.string :current_course
       t.integer :current_lesson
--- phase-3-sinatra-react-project-backend/db/migrate/20230110220126_create_tutors.rb
+++ phase-3-sinatra-react-project-backend/db/migrate/20230110220126_create_tutors.rb
@@ -1,6 +1,7 @@
 class CreateTutors < ActiveRecord::Migration[6.1]
   def change
     create_table :tutors do |t|
+      t.string :position
       t.string :name
       t.string :courses
       t.string :image_url
