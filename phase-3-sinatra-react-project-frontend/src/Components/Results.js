790760df2e99599be9ec8c23ff194de9ae8019f3
Steven McVey <smcvey1210@gmail.com>
2023-01-15 17:40:17 -0800

    work on user tiles in search results

--- phase-3-sinatra-react-project-frontend/src/Components/Results.js
+++ phase-3-sinatra-react-project-frontend/src/Components/Results.js
@@ -1,12 +1,185 @@
 import React from "react";
 import UserTile from "./UserTile";
 
-function Results(results){
+function Results(){
+
+  const results = [
+    {
+      position: "student",
+      name: "Test Student",
+      current_course: "Elvish 203",
+      current_lesson: 6,
+      username: "teststudent",
+      password:  "password"
+    },
+    {
+      position: "student",
+      name: "Pamela Louise",
+      current_course: "German 101",
+      current_lesson: 8,
+      username: "pamelalouise",
+      password:  "password"
+    },
+    {
+      position: "student",
+      name: "David Ward",
+      current_course: "Pig Latin 303",
+      current_lesson: 3,
+      username: "davidward",
+      password:  "password",
+    },
+    {
+      position: "student",
+      name: "Ally Irene",
+      current_course: "Canadian 101",
+      current_lesson: 4,
+      username: "allyirene",
+      password:  "password"
+    },
+    {
+      position: "student",
+      name: "Paul Herman",
+      current_course: "Elvish 202",
+      current_lesson: 10,
+      username: "paulherman",
+      password:  "password"
+    },
+    {
+      position: "student",
+      name: "Yiyi Hou",
+      current_course: "Chinese 203",
+      current_lesson: 13,
+      username: "yiyihou",
+      password:  "password"
+    },
+    {
+      position: "student",
+      name: "Henry Hou",
+      current_course: "Korean 101",
+      current_lesson: 3,
+      username: "henryhou",
+      password:  "password"
+    },
+    {
+      position: "student",
+      name: "Chris Charles",
+      current_course: "German 102",
+      current_lesson: 7,
+      username: "chrischarles",
+      password:  "password"
+    },
+    {
+      position: "student",
+      name: "Staci Jean",
+      current_course: "Spanish 402",
+      current_lesson: 8,
+      username: "stacijean",
+      password:  "password"
+    },
+    {
+      position: "student",
+      name: "Erich James",
+      current_course: "Spanish 201",
+      current_lesson: 12,
+      username: "erichjames",
+      password:  "password"
+    },
+    {
+      position: "student",
+      name: "George James",
+      current_course: "Spanish 302",
+      current_lesson: 8,
+      username: "georgejames",
+      password:  "password"
+    },
+    {
+      position: "tutor",
+      name: "Whitney",
+      courses: "Chinese and Korean",
+      image_url: "https://m.media-amazon.com/images/M/MV5BMTkyMzA1NTk1M15BMl5BanBnXkFtZTcwMTYxNTkzNw@@._V1_.jpg",
+      username: "steven",
+      password: "password",
+    },
+    {
+      position: "tutor",
+      name: "Angie",
+      courses: "Spanish and French",
+      image_url: "https://www.themoviedb.org/t/p/w500/k3W1XXddDOH2zibPkNotIh5amHo.jpg",
+      username: "angie",
+      password: "password",
+    },
+    {
+      position: "tutor",
+      name: "Legolas",
+      courses: "Elvish",
+      image_url: "https://static.wikia.nocookie.net/the-hobbit-lotr-trilogies/images/c/c0/Legolas2.jpg/revision/latest?cb=20190721145737",
+      username: "legolas",
+      password: "password",
+    },
+    {
+      position: "tutor",
+      name: "Brad",
+      courses: "German, French, and Pig Latin",
+      image_url: "https://m.media-amazon.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_.jpg",
+      username: "brad",
+      password: "password",
+    },
+    {
+      position: "tutor",
+      name: "Hayao",
+      courses: "Japanese",
+      image_url: "https://upload.wikimedia.org/wikipedia/commons/e/e2/Hayao_Miyazaki_cropped_1_Hayao_Miyazaki_201211.jpg",
+      username: "hayao",
+      password: "pasword",
+    },
+    {
+      position: "tutor",
+      name: "Katya",
+      courses: "Russian",
+      image_url: "https://nationaltoday.com/wp-content/uploads/2022/10/456841067-min-1.jpg",
+      username: "katya",
+      password: "password",
+    },
+    {
+      position: "tutor",
+      name: "Ariana",
+      courses: "Polish and Ukrainian",
+      image_url: "https://www.biography.com/.image/t_share/MTQ3MzM3MTcxNjA5NTkzNjQ3/ariana_grande_photo_jon_kopaloff_getty_images_465687098.jpg",
+      username: "ariana",
+      password: "password",
+    },
+    {
+      position: "tutor",
+      name: "Julia",
+      courses: "Czech",
+      image_url: "https://flxt.tmsimg.com/assets/1474_v9_bb.jpg",
+      username: "julia",
+      password: "password",
+    },
+    {
+      position: "tutor",
+      name: "Hugh",
+      courses: "Australian and Canadian",
+      image_url: "https://m.media-amazon.com/images/M/MV5BNDExMzIzNjk3Nl5BMl5BanBnXkFtZTcwOTE4NDU5OA@@._V1_.jpg",
+      username: "hugh",
+      password: "password",
+    },
+    {
+      position: "tutor",
+      name: "emily",
+      courses: "Portuguese",
+      image_url: "https://m.media-amazon.com/images/M/MV5BMTUxNDY4MTMzM15BMl5BanBnXkFtZTcwMjg5NzM2Ng@@._V1_.jpg",
+      username: "emily",
+      password: "password",
+    }
+  ]
 
   return(
     <div>
       <h2>Results</h2>
-      {/* {results.map(user=><UserTile user = {user}/>)} */}
+      <div className="flexContainer">
+        {results.map(user=><UserTile key={user.username} user = {user}/>)}
+      </div>
     </div>
   )
 }
--- phase-3-sinatra-react-project-frontend/src/Components/UserTile.js
+++ phase-3-sinatra-react-project-frontend/src/Components/UserTile.js
@@ -3,8 +3,24 @@ import React from "react";
 function UserTile({user}){
 
   return(
-    <div>
+    <div className="tile">
+      <h3 id={user.position === "student" ? "studentPosition" : "tutorPosition"}>{user.position}</h3>
       <h3>{user.name}</h3>
+      <div>
+      {user.position === "student" ? 
+        <div>
+          <h4>({user.username})</h4>
+          <p>Course: {user.current_course}</p>
+          <p>Lesson: {user.current_lesson} </p>
+        </div> : 
+        <div>
+          <img className="profilePic" src={user.image_url}></img>
+          <p>Courses offered: {user.courses}</p>
+          <button>Book a lesson!</button>
+        </div>
+      }
+      </div>
+      
     </div>
   )
 }
